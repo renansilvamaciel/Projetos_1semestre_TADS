445dfb2613c9a45f2e1196747fbf3a2c92148870
I <I@192.168.0.10>
2019-11-18 01:05:31 -0200

    exercio 16 90% ps: fazer gabiarra

---
+++ lista_exercicios_04/src/src/list_ex_4/ex_16.java
@@ -0,0 +1,57 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package list_ex_4;
+
+/**
+ *
+ * @author renan.smaciel
+ */
+public class ex_16 // quase pronto falta fazer gabiarra.
+{
+    static int [] vetorOrdenadoPar (int [] vetA)
+    {
+        int aux;
+        for (int j = 0; j < vetA.length; j++) 
+            {
+                for (int i = 0; i < vetA.length-1; i++) 
+                    {
+                        if (vetA[i]%2==0)
+                            {
+                                System.out.println("oi");
+                               aux = vetA[j];
+                                vetA[j] = vetA[i];
+                                vetA[i] = aux;
+                                break;
+                            }
+                    }
+             }
+        return vetA;
+    }
+    
+    
+    static void imprimirTest (int [] vetA)
+    {
+            System.out.println(" vetor baguncado : \n");
+            for (int item : vetA)
+            {
+                System.out.print(item+" ");
+            }
+            
+            System.out.println("  \n");
+    }
+    
+    public static void main(String[] args) 
+    {
+        int t = ex_1.tamanho();
+        int [] vetA = ex_1.criaVetor(t);
+        vetA = ex_1.preencheVet(vetA);
+        imprimirTest(vetA);
+        vetA = vetorOrdenadoPar(vetA);
+        ex_1.imprimirVetor(vetA);
+        
+        
+    }
+}
